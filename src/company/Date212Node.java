cd0ee2ea685707057d62efe06000ebb07381cd4e
Nick Tsang <nick.tsang5@gmail.com>
2021-11-19 11:01:43 -0500

    Project 2 Java Date 212

--- src/company/Date212Node.java
+++ src/company/Date212Node.java
@@ -1,4 +1,11 @@
 package company;
 
 public class Date212Node {
+    protected Date212 data;
+    protected Date212Node next;
+
+    public Date212Node(Date212 date){
+        data = date; // data is set to the date being passed in
+        next = null; // data is pointed to null;
+    }
 }
