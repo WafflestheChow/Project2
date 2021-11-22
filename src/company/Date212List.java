b6120970412769dcb97bdc57f55a2a69c9549431
Nick Tsang <nick.tsang5@gmail.com>
2021-11-21 20:04:35 -0500

    Project 2 Java Date 212

--- src/company/Date212List.java
+++ src/company/Date212List.java
@@ -1,5 +1,17 @@
 package company;
 
-public class Date212List {
+public abstract class Date212List {
+
+    //Set up first node as a dummy node;
+    private Date212Node first = new Date212Node(null);
+    // Set up last node as first when called.
+    private Date212Node last = first;
+
+    private int length = 0;
+
+    public int getLength(){
+        return length;
+    }
+
 
 }
